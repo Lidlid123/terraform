f9bc836f3f8dabecd8de395a123efc1308cf2d8e
Your Name <youremail@example.com>
2023-04-09 22:05:08 +0200

    first (:

---
+++ excer3/instances.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ebd1b97f22a7a683cdaf4c885ae44b25d70c9c0bfc7adefa1e3ec28a7e9d8f1d
+size 840
---
+++ excer3/vars.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b29f67291229a7f7c4afe2092d8349d211b35f37087ffa89d50a1d13c7ab2b98
+size 309
